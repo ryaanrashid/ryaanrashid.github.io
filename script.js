89dc458c1e0662466e4df15748f0f16629c51f79
ryaanrashid <ryaan.rashid@hotmail.com>
2020-08-16 11:54:41 +1000

    Changing from TicTacToe to Codenames
    uploading html css and js files to change the site from tictactoe game to codenames.
    
    so far added:  random unique words and new game button

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
-const X_CLASS = 'x'
-const C_CLASS = 'circle'
-const WINNING_COMBOS = [
-	[0,1,2],
-	[3,4,5],
-	[6,7,8],
-	[0,3,6],
-	[1,4,7],
-	[2,5,8],
-	[0,4,8],
-	[2,4,6]
-]
+const wordList = ["soldier", "trip", "tap", "pole", "spike", "conductor", "penguin", "washington", "bed", "crown", "hole", "ketchup", "grass", "buck", "himalayas", "hotel", "boom", "robot", "apple", "cricket", "water", "well", "olive", "fly", "cycle"]
+
 const cellElements = document.querySelectorAll('[data-cell]')
 const winningMessageElement = document.getElementById('winningMessage')
 const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
-const restartButton = document.getElementById('restartButton')
+const newGameButton = document.getElementById('newGameButton')
 let circleTurn
 
 cellElements.forEach(cell => {
 	cell.addEventListener('click', handleClick, {once:true})
 })
 
-restartButton.addEventListener('click', startGame)
+newGameButton.addEventListener('click', startGame)
 
 function startGame() {
 	circleTurn = false
+	// populate cells with words
+	fillCells()
 	cellElements.forEach(cell => {
-		cell.classList.remove(X_CLASS)
-		cell.classList.remove(C_CLASS)
+		cell.classList.remove('clicked')
 		cell.addEventListener('click', handleClick, {once:true})
 	})
 	winningMessageElement.classList.remove('show')
@@ -34,45 +25,29 @@ function startGame() {
 
 function handleClick(e) {
 	const cell = e.target
-	const currentClass = circleTurn ? C_CLASS : X_CLASS
-	placeMark(cell, currentClass)
-	if (checkWin(currentClass)) {
-		endGame(false)
-	} else if (isDraw()) {
-		endGame(true)
-	} else {
-		swapTurns()
+	cell.classList.add('clicked')
+}
+
+function addWord(cell, word) {
+	cell.innerHTML = word
+}
+
+function fillCells() {
+	// Generate the sequence of words from the wordlist being used
+	words = []
+	while (words.length < wordList.length) {
+		newWord = Math.floor(Math.random() * wordList.length)
+		if(words.includes(newWord)){
+		} else {
+			words.push(newWord)
+		}
 	}
-}
 
-function endGame(draw) {
-	if (draw) {
-		winningMessageTextElement.innerText = "Draw!"
-	} else {
-		winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
-	}
-	winningMessageElement.classList.add('show')
-}
-
-function isDraw() {
-	return [...cellElements].every(cell => {
-		return cell.classList.contains(X_CLASS) ||
-		cell.classList.contains(C_CLASS)
+	// Use the above sequence to fill cells with their word
+	i = 0
+	cellElements.forEach(cell => {
+		cell.innerHTML = wordList[words[i]]
+		i += 1
 	})
-}
 
-function placeMark(cell, currentClass) {
-	cell.classList.add(currentClass)
 }
-
-function swapTurns() {
-	circleTurn = !circleTurn;
-}
-
-function checkWin(currentClass){
-	return WINNING_COMBOS.some(combination => {
-		return combination.every(index => {
-			return cellElements[index].classList.contains(currentClass)
-		})
-	})
-} 
